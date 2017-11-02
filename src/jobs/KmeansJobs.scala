de9eb56fcaa61e7f7107eb38ba443f814cbacaf1
Manthan Thakar <manthant15@gmail.com>
2017-11-02 19:07:49 -0400

    Create a driver class to call kmeans jobs

---
+++ src/jobs/KmeansJobs.scala
@@ -0,0 +1,23 @@
+package jobs
+
+import clustering.Kmeans
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import record.SongRecord
+
+class KmeansJobs {
+
+  def execute(sc: SparkContext, songData: RDD[SongRecord]) : Unit = {
+    val fuzzyLoudness = new Kmeans(sc, songData.map(_.loudness), 10)
+    fuzzyLoudness.run("output/kmeans-loudness")
+
+    val fuzzyLength = new Kmeans(sc, songData.map(_.duration), 10)
+    fuzzyLength.run("output/kmeans-length")
+
+    val fuzzyTempo = new Kmeans(sc, songData.map(_.tempo), 10)
+    fuzzyTempo.run("output/kmeans-tempo")
+
+    val fuzzyHotness = new Kmeans(sc, songData.map(_.songHotness), 10)
+    fuzzyHotness.run("output/kmeans-hotness")
+  }
+}
